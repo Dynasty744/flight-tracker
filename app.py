827f28c53e07c8a6248fc6b4aa79583d444c08cf
Dynasty744 <jessyang650@gmail.com>
2025-02-27 12:52:49 -0800

    implement flights api call

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+from flask_cors import CORS
+import requests
+import os
+
+load_dotenv
 
 app = Flask(__name__)
+CORS(app)
+
+AVIATIONSTACK_API_URL = "http://api.aviationstack.com/v1"
+API_KEY = os.getenv('API_KEY')
 
 @app.route('/')
 def home():
-  return jsonify({"message": "Welcome to Flight Tracker!"})
+    return "Welcome to the Flight Tracker API! Use the /flights endpoint to search for flights."
+
+@app.route('/flights', methods=['GET'])
+def get_flights():
+  airline = request.args.get('airline') # get airline code from query params
+  params = {
+        'access_key': API_KEY,
+        'airline_name': airline,
+        'flight_status': 'active'  # fetch only active flights
+    }
+  response = requests.get(f"{AVIATIONSTACK_API_URL}/flights", params=params)
+  if response.status_code != 200:
+     return jsonify({"error": "Failed to fetch data from AviationStack"}), 500
+  data = response.json()
+  return jsonify(data['data'])
 
 if __name__ == '__main__':
   app.run(debug=True)
